aece982b719fe69dfb422c14e86eeae9e68c895f
BRAVO <blessonthomas2517766@gmail.com>
2025-04-15 08:00:24 +0530

    .

--- module 5/plot.py
+++ module 5/plot.py
@@ -1,5 +1,9 @@
-import numpy as np
+'''import numpy as np
 import matplotlib.pyplot as plt
 
 plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+'''
+import matplotlib.pyplot as plt
+plt.bar([1,2,3],[3,2,6])
 plt.show()
