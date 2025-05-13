717fb2ad8fe8fd19a54c192d49b9a09a99576367
BRAVO <blessonthomas2517766@gmail.com>
2025-05-13 18:10:58 +0530

    ..

--- prevyr/test.py
+++ prevyr/test.py
@@ -1,6 +1,10 @@
-class Car:
-    def __init__(self,reg,col,mil,year):
-        self.reg=reg
-        self.col=col
-        self.mil=mil
-        self.year
+import pandas as pd
+
+data={
+    'student_id':[12,44,33],
+    'name':['Blesson','Jerin','Jibu'],
+    'gender' :['m','m','m'],
+    'birthdate':['2022-06-24','2023-05-01','2022-06-24'],
+    'grade':['A','A','A']
+}
+df=pd.DataFrame(data)
